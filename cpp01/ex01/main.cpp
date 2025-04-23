e0c973926de64a4d95f6ac1453154d82c42beeec
Abdelaadim Bougurine <abouguri@c3r8p7.1337.ma>
2025-04-23 16:09:21 +0100

    ex01

---
+++ cpp01/ex01/main.cpp
@@ -0,0 +1,23 @@
+#include "Zombie.hpp"
+#include <sstream>
+
+int main()
+{
+    int N = 5;
+    std::string baseName = "Horde Zombie";
+    
+    std::cout << "Creating a zombie horde of " << N << " zombies" << std::endl;
+    
+    Zombie* horde = zombieHorde(N, baseName);
+    
+    for (int i = 0; i < N; i++) {
+        std::cout << "Zombie #" << i + 1 << ": ";
+        horde[i].announce();
+    }
+    
+    std::cout << "\nDestroying the zombie horde" << std::endl;
+
+    delete[] horde;
+    
+    return 0;
+}
---
+++ cpp01/ex01/zombieHorde.cpp
@@ -0,0 +1,16 @@
+#include "Zombie.hpp"
+
+Zombie* zombieHorde(int N, std::string name) {
+    if (N <= 0) {
+        std::cout << "Cannot create a horde with " << N << " zombies!" << std::endl;
+        return NULL;
+    }
+    
+    Zombie* horde = new Zombie[N];
+    
+    for (int i = 0; i < N; i++) {
+        horde[i].setName(name);
+    }
+    
+    return horde;
+}
